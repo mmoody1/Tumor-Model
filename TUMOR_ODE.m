e5bf35e8fa85318b1ceccd83b997671ceb5ca76e
mmoody1 <70147548+mmoody1@users.noreply.github.com>
2022-03-17 11:32:47 -0500

    Update TUMOR_ODE.m

--- TUMOR_ODE.m
+++ TUMOR_ODE.m
@@ -28,4 +28,4 @@ function Np = TUMOR_ODE(t,N,flag,s,d1,r1,r2,b1,b2,c1,c2,c3,c4,alpha,ro)
 
     Np(1) = r2*X*(1-b2*X)-c4*T*X;  %normal cells
     Np(2) = r1*T*(1-b1*T)-c2*I*T-c3*T*X; %tumor cells 
-    Np(3) = s+(ro*I*T/alpha+T)-c1*I*T-d1*I; %immune cells
+    Np(3) = s+((ro*I*T)/(alpha+T))-c1*I*T-d1*I; %immune cells

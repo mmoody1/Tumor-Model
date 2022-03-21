42e089f16d694e09e8bf2e0d94339e40e4edbbab
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-03-20 22:02:19 -0500

    Update TUMOR_ODE2.m

--- TUMOR_ODE2.m
+++ TUMOR_ODE2.m
@@ -20,13 +20,18 @@ function Np = TUMOR_ODE2(t,N,flag,s,d1,d2,r1,r2,b1,b2,c1,c2,c3,c4,a1,a2,a3,alpha
      % N(1) is the normal cell population = X
      % N(2) is the tumor cell population = T
      % N(3) is the immune cell population = I
-  Np=zeros(size(N));
+     % N(4) is the per capita decay of the drug once injected
+     
+ Np=zeros(size(N));
     X=N(1);
     T=N(2);
     I=N(3);
     u=N(4);
+%   
 
     Np(1) = r2*X*(1-b2*X)-c4*T*X-a3*(1-exp(-u))*X;
     Np(2) = r1*T*(1-b1*T)-c2*I*T-c3*T*X-a2*(1-exp(-u))*T;
     Np(3) = s+((ro*I*T)/(alpha+T))-c1*I*T-d1*I-a1*(1-exp(-u))*I;
-    Np(4) = v-d2*u;
+    Np(4) = v.*abs(cos(0.4*t))-d2*u; %pulsed chemotherapy at 12 cycles in three months
+             
+end

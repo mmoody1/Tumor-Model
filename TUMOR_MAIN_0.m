98ea87b4507c44a54db8e79652708237c44e54f1
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-03-24 16:09:09 -0500

    Update TUMOR_MAIN_0.m

--- TUMOR_MAIN_0.m
+++ TUMOR_MAIN_0.m
@@ -1,9 +1,10 @@
-function TUMOR_MAIN_0
+function TUMOR_MAIN
 %tumor model
 clear all
 clc
 clf
 %input parameter values
+
 s = 0.1181; % Constant immune cells source rate (#cells/day)
 sigma = 20.19; % Steepness coefficient (#cells/day)
 ro = 1.131; % Tumor recruitment rate of immune cells (1/day) 
@@ -14,15 +15,15 @@ b = 0.002; %inverse carrying capacity of tumor population (#cells)
 c2 = 1; %Immune kill rate of tumor cells (1/cell*day)
 
 
-
 N10 = 0.00001;   % Initial Immune cell population
-N20 = 10;        % Initial tumor cell population for a large tumor
+N20 = 1;        % Initial tumor cell population for a large tumor
 %N20= 1;         % Initial tumor cell population for a small tumor
-tend = 50;       % Simulation length (time)
+tend = 150;       % Simulation length (time)
 
-N0 = [N10 N20];
+N0=[N10 N20];
 
- [t, N] = ode23s('TUMOR_ODE_0',[0 tend], N0,[],s,d1,c1,a,b,c2,sigma,ro);
+
+[t,N] = ode45('TUMOR_ODE',[0 tend],N0,[],s,d1,a,b,c1,c2,sigma,ro);
 
 subplot(2,1,1)
 plot(t,N(:,1));
@@ -30,9 +31,10 @@ plot(t,N(:,1));
     ylabel('Immune cells')
 
 subplot(2,1,2)
-plot(t,N(:,2));
+plot(t,N(:,2),'m');
     xlabel('time')
     ylabel('tumor cells')
 
-legend('Immune cells', 'Tumor Cells')
+%legend('Immune cells', 'Tumor Cells')
 
+end

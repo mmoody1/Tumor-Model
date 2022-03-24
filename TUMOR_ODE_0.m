8f0654b534f815dfbb65c6db99359dde2c3596f0
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-03-24 12:22:27 -0500

    Update TUMOR_ODE_0.m

--- TUMOR_ODE_0.m
+++ TUMOR_ODE_0.m
@@ -2,31 +2,25 @@ function Np = TUMOR_ODE_0(t,N,flag,s,d1,c1,a,b,c2,sigma,ro)
    %TUMOR_ODE defines the ODEs for the Tumor Model
    
    % INPUT parameters:
-     % t is time (days)
-     % s is the influx of immune cells when tumor cells present
-     % d1 is the death rate of immune cells in the absence of tumors
-     % r1 is the growth rate associated with tumor cells
-     % r2 is the growth rate associated with normal cells
-     % b1 is the reciprocol carrying capacities for tumor cells
-     % b2 is the reciprocol carrying capacities for normal cells
-     % c1 is the competitive coefficient between tumor on immune cells
-     % c2 is the competitive coefficient between immune on tumor cells
-     % c3 is the competitive coefficient between normal on tumor cells
-     % c4 is the competitive coefficient between tumor on normal cells
-     % alpha is the immune threshold rate
-     % ro is the immune response rate  
+     %s is constant immune cells source rate (#cells/day)
+% sigma is the steepness coefficient (#cells/day)
+% ro is the tumor recruitment rate of immune cells (1/day) 
+% c1 is the tumor deactivation rate of immune cells (1/cell*day)
+% d1 is the natural death rate of immune cells (1/day)
+% a is the intrinsic tumor growth rate (1/day)
+% b is the inverse carrying capacity of tumor population (#cells)
+% c2 is the immune kill rate of tumor cells (1/cell*day)
    
-    % OUTPUT:
-     % N(1) is the immune cell population = I
-     % N(2) is the tumor cell population = T
+% OUTPUT:
+% N(1) is the immune cell population = I
+% N(2) is the tumor cell population = T
      
      
-     Np=zeros(size(N));
-    %X=N(1); %normal cell population
+Np=zeros(size(N));
     T=N(1); %tumor cell population
     I=N(2); %immune cell population
 
-    %Np(1) = r2*X*(1-b2*X)-c4*T*X;  %normal cells
-    Np(1) = s+((ro*I*T)/(sigma+T))-c1*I*T-d1*I; %immune cells
-    Np(2) = a*T*(1-b*T)-c2*I*T; %tumor cells 
+  
+    Np(1) = s+((ro*I*T)/(sigma+T))-c1*I*T-d1*I; %immune cells equation
+    Np(2) = a*T*(1-b*T)-c2*I*T; %tumor cells equation
     

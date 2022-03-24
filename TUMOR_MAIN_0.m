bc077c8f2a9aacbe23a264b3a6b7d36eedd6e6f4
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-03-24 11:31:14 -0500

    Update TUMOR_MAIN_0.m

--- TUMOR_MAIN_0.m
+++ TUMOR_MAIN_0.m
@@ -5,14 +5,14 @@ clc
 clf
 %  Np(1) = s+((ro*I*T)/(sigma+T))-c1*I*T-d1*I; %immune cells
 %     Np(2) = a*T*(1-b*T)-c2*I*T; %tumor cells
-s = 0.1181 %
-sigma = 20.19; %
-ro = 1.131; %
-c1 = 0.00311; %
-d1 = 0.3743;
-a = 1.636;
-b = 0.002;
-c2 = 1;
+s = 0.1181 % Constant immune cells source rate (#cells/day)
+sigma = 20.19; % Steepness coefficient (#cells/day)
+ro = 1.131; % Tumor recruitment rate of immune cells (1/day) 
+c1 = 0.00311; % Tumor deactivation rate of immune cells (1/cell*day)
+d1 = 0.3743; %natural death rate of immune cells (1/day)
+a = 1.636; %Intrinsic tumor growth rate (1/day)
+b = 0.002; %inverse carrying capacity of tumor population (#cells)
+c2 = 1; %Immune kill rate of tumor cells (1/cell*day)
 
 
 
@@ -36,37 +36,4 @@ plot(t,N(:,2));
 
 %legend('Immune cells', 'Tumor Cells')
 
-
-%  %%plotting nullcines
-% figure
-% 
-% T = linspace(0,1,20); 
-%  N = linspace(0,1,20);
-%  I = linspace(0,1,20);
-% 
-%  %nullsurface of N
-%  [T1, I1] = meshgrid(T, I);
-% Nn= 1/b2 - (c4/(r2*b2).*T);
-% surf(Nn,T1, I1)
-% hold on
-% 
-% %nullsurface of T
-%  [N1, I1] = meshgrid(N, I);
-% Nt = 1/b2 - c2/(r1*b1).*I - (c3/(r1*b1))*N 
-% surf(N1, Nt, I1)
-% 
-%  hold on
-% [N1, T1] = meshgrid(N, T);
-% Ni =   (s.*(sigma + T))./((c1.*T + d1).*(sigma + T) - ro.*T) %nullsurface of I
-%  surf(N1, T1, Ni)
-% 
-% surf(N, Nt, I)
-% xlabel('Normal cells')
-% ylabel('Tumor cells')
-% zlabel('Immune cells')
-%  hold off
-% 
-% % axis('equal')
-%  
-% 
 

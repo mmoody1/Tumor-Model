6b23b6762690918a35bcea0a2deb525027c3b3ad
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-03-24 17:07:38 -0500

    Update TUMOR_MAIN_0.m

--- TUMOR_MAIN_0.m
+++ TUMOR_MAIN_0.m
@@ -1,4 +1,5 @@
 function TUMOR_MAIN
+opengl software
 %tumor model
 clear all
 clc
@@ -25,16 +26,39 @@ N0=[N10 N20];
 
 [t,N] = ode45('TUMOR_ODE',[0 tend],N0,[],s,d1,a,b,c1,c2,sigma,ro);
 
-subplot(2,1,1)
+subplot(3,1,1)
 plot(t,N(:,1));
     xlabel('time')
     ylabel('Immune cells')
 
-subplot(2,1,2)
+subplot(3,1,2)
 plot(t,N(:,2),'m');
     xlabel('time')
     ylabel('tumor cells')
+ 
+subplot(3,1,3)
+plot(N(:,1),N(:,2),'r');
+    xlabel('Immune cells')
+    ylabel('Tumor cells')
 
-%legend('Immune cells', 'Tumor Cells')
+ figure
+%Nullclines
+I1 = linspace(0,5,11);
+T1 = linspace(0,500,11);
+
+%Nullcline of Immune cells
+Ni = s.*(sigma + T1)./((c1.*T1).*(sigma + T1) + d1.*(sigma+T1)-ro.*T1);
+Ti= (a.*(1-b.*T1))./c2 ;
+plot(I1,Ni,'m');
+hold on
+plot(I1,Ti,'r');
+xlabel('Immune cell population');
+ylabel('Tumor Cell population');
+legend('Immune cells', 'Tumor Cells')
+title('Immune cells vs Tumor cells')
+plot (2.5,0.818, 'rx'); %still working on nailing that fixed point...also one more i havent been able to highlight yet
+plot(4.5,0.1636, 'rx');
+hold off
+
 
 end

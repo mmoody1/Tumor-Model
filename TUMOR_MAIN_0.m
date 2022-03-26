735a3fdde4f7f46b515d2f9a86cac95d38e07edd
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-03-26 15:54:52 -0500

    Update TUMOR_MAIN_0.m

--- TUMOR_MAIN_0.m
+++ TUMOR_MAIN_0.m
@@ -1,5 +1,5 @@
 function TUMOR_MAIN
-% opengl software
+ opengl software
 %tumor model
 clear all
 clc
@@ -20,7 +20,7 @@ c2 = 1; %Immune kill rate of tumor cells (1/cell*day)
 N10 = 0.00001;   % Initial Immune cell population
 N20 = 1;        % Initial tumor cell population for a small tumor burden (x10^6)
 %N20= 10;         % Initial tumor cell population for a large tumor burden
-tend = 150;       % Simulation length (time)
+tend = 60;       % Simulation length (time)
 
 N0=[N10 N20];
 
@@ -30,17 +30,29 @@ N0=[N10 N20];
 subplot(3,1,1)
 plot(t,N(:,1));
     xlabel('time')
-    ylabel('Immune cells')
+    ylabel('Immune cells (X10^6)')
 
 subplot(3,1,2)
 plot(t,N(:,2),'m');
     xlabel('time')
-    ylabel('tumor cells')
+    ylabel('tumor cells (X10^6)')
  
 subplot(3,1,3)
 plot(N(:,1),N(:,2),'r');
-    xlabel('Immune cells')
-    ylabel('Tumor cells')
+    xlabel('Immune cells (X10^6)')
+    ylabel('Tumor cells (X10^6)')
+
+ figure %plots Immune and tumor cell growth
+ plot(t,N(:,1));
+hold on
+plot(t,N(:,2),'m');
+
+xlabel('Time (days)')
+ylabel('Number of cells (X10^6)')
+legend('Immune cells', 'Tumor cells')
+ylim([0 215]);
+
+hold off
 
  figure
 %Plotting Nullclines and indicating fixed points
@@ -56,15 +68,18 @@ ylim([100 1000]); %chopped off unwanted part of that curve so the graph fits bet
   hold on
  plot(Nullcline2, T1,'r'); 
 hold on
+
 %fixed points drawn as an x
 plot (0.181778,444.444, 'kx'); 
 plot(0.743636,272.727, 'kx');
 plot(0.97499,202.02, 'kx');
 
+%labeling and legend
 xlabel('Immune cell population (X10^6)');
 ylabel('Tumor Cell population (X10^6)');
 legend('N1 (dI/dt = 0)', 'N2 (dT/dt = 0)', '(0.181, 444.44) Fixed point', ' (0.744, 272.73) Fixed point','(0.97499,202.02) Fixed point')
 title('Immune cells vs Tumor cells')
+  
 hold off
 
 

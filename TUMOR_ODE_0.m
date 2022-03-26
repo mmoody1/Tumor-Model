e52389095ff3ce952c933c8691d2717995ddb122
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-03-26 15:53:55 -0500

    Update TUMOR_ODE_0.m

--- TUMOR_ODE_0.m
+++ TUMOR_ODE_0.m
@@ -3,7 +3,7 @@ function Np = TUMOR_ODE(t,N,flag,s,d1,a,b,c1,c2,sigma,ro)
    
    % INPUT parameters:
      %s is constant immune cells source rate (#cells/day)
-% alpha is the steepness coefficient (#cells/day)
+% sigma is the steepness coefficient (#cells/day)
 % ro is the tumor recruitment rate of immune cells (1/day) 
 % c1 is the tumor deactivation rate of immune cells (1/cell*day)
 % d1 is the natural death rate of immune cells (1/day)
@@ -17,11 +17,12 @@ function Np = TUMOR_ODE(t,N,flag,s,d1,a,b,c1,c2,sigma,ro)
      
      
 Np=zeros(size(N));
-    T=N(1); %tumor cell population
-    I=N(2); %immune cell population
+    I=N(1); %tumor cell population
+    T=N(2); %immune cell population
 
   
     Np(1) = s+(ro*I*T)/(sigma+T)-c1*I*T-d1*I;    %immune cells equation
     Np(2) = a*T*(1-b*T)-c2*I*T;                    %tumor cells equation
 
 end
+    

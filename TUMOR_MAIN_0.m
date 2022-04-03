195e82bd8a21c87845dcce66c5682790f778e91f
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-04-02 19:10:35 -0500

    Update TUMOR_MAIN_0.m

--- TUMOR_MAIN_0.m
+++ TUMOR_MAIN_0.m
@@ -1,4 +1,3 @@
-%  opengl software
 % Tumor model
 
 clear all
@@ -23,8 +22,8 @@ T1 = linspace(212,1000,100);
 T2 = linspace(1,10,100);
 T3 = linspace(1,1000,100);
 
-% N
-%  ullcline of Immune cells
+
+%  Nullclines
 Nullcline1a = s.*(sigma + T1)./((c1.*T1 + d1).*(sigma + T1)-ro.*T1); %dI/dt = 0
 Nullcline1b = s.*(sigma + T2)./((c1.*T2 + d1).*(sigma + T2)-ro.*T2); %dI/dt = 0
 Nullcline2= (a.*(1-b.*T3))./c2 ; %dT/dt = 0
@@ -34,32 +33,81 @@ hold on
 plot(Nullcline1a, T1,'c');
 plot(Nullcline1b, T2,'c');
 xlim([0 3]); 
- ylim([0 1000]); %re-scaled
-  hold on
+ ylim([0 1000]);
+ 
  plot(Nullcline2, T3,'k--'); 
- set(gca, 'YScale', 'log');
- set(gca, 'XScale', 'linear');
-  hold on
+ set(gca, 'YScale', 'log'); %log scale for tumor axis
+ set(gca, 'XScale', 'linear'); %linear scale for immune axis
+
+
+%  fixed points indicated as 'o'
+plot (0.181778,444.444444444444, 'ro'); 
+plot(0.763467,266.667, 'bo');
+plot(1.60427,8.2976, 'ro');
 
 % % labeling and legend
 xlabel('Immune cell population (X10^6)');
 ylabel('Tumor Cell population (X10^6)');
-lgd = legend('N1 (dI/dt = 0)', 'N2 (dT/dt = 0)','I vs T', 'A(0.181, 444.44) Stable', ' B(0.744, 272.73) Unstable','C(0.97499,202.02) Unstable', 'D(1.58656,14.2431) Unstable');
+lgd = legend('N1a (dI/dt = 0)', 'N1b (dI/dt = 0)', 'N2 (dT/dt = 0)', 'A(0.181, 444.44) Stable', ' B(0.744, 272.73) Unstable', 'C(1.60427,8.2976) Stable','I vs T');
 lgd.FontSize = 5;
-title('Nullclines');    
+title('Nullclines and Trajectory cell growth');    
 
-N10 = linspace(0.1,0.5,3);      % Initial Immune cell population
-N20 = linspace(0.1,20,3);         % Initial tumor cell population for a small tumor burden (x10^6)
-%N20 = 10;          % Initial tumor cell population for a large tumor burden
-tend = 150;         % Simulation length (time)
+N10 = linspace(0.00001,0.1,2);      % Initial Immune cell population
+N20 = linspace(0.1,15,2);         % Initial tumor cell population - tumor burden (x10^6)
 
+tend = 90;         % Simulation length (days)
+%sweeping through initial cell populations for immune and tumor
 for i = 1:length(N20)
     for j = 1:length(N10)
         N0=[N10(j) N20(i)];
 
         [t,N] = ode45('TUMOR_ODE',[0 tend],N0,[],s,d1,a,b,c1,c2,sigma,ro); 
-        plot(N(:,1), N(:,2));
+         plot(N(:,1), N(:,2)); %plots immune vs tumor
     end
 end
 hold off
+
+%Population growth plots
+figure
+subplot(3,1,1)
+plot(t,N(:,1));
+    xlabel('time')
+    ylabel('Immune cells (X10^6)')
+xlim([0 90]);
+
+subplot(3,1,2)
+
+plot(t,N(:,2),'m');
+    xlabel('time')
+    ylabel('tumor cells (X10^6)')
+ xlim([0 90]);
+
+ subplot(3,1,3)
+plot(N(:,1),N(:,2),'r');
+    xlabel('Immune cells (X10^6)')
+    ylabel('Tumor cells (X10^6)')
+
+    %Jacobian matrix and eigen values calculation.
+
+%Fixed points values (I,T)
+I_eig = 0.181778;
+T_eig = 444.44444; ;
+
+%I_eig = 1.60427;
+%T_eig = 8.2976;
+
+%I_eig = 0.763467;
+%T_eig = 266.667; 
+
+%I_eig = 1.60427
+%T_eig = 7.95588
+
+
+%Jacobian matrix [dI/dI, dI/dT; dT/dI, dT/dT] 
+Jac = [1.131*T_eig/(20.19+T_eig)-0.00311*T_eig-0.3743, 1.131*I_eig/(20.19+T_eig) - (20.19+T_eig)^(-2) - 0.00311*I_eig; -T_eig, 1.636-0.014544*T_eig-I_eig];
+
+lamda = eig(Jac); %lamda(1,2)<0 stable, lamda(1,2)>0 unstable
+
+ 
+
 

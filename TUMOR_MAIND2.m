4176334e88814231e0d89d481b7ad741a0764a13
GWillsKpeli <98865615+GWillsKpeli@users.noreply.github.com>
2022-04-04 20:37:25 -0500

    Update TUMOR_MAIND2.m

--- TUMOR_MAIND2.m
+++ TUMOR_MAIND2.m
@@ -1,79 +1,44 @@
-opengl software
+function Np = TUMOR_ODED2(t,N,flag,s,d1,d2,a,b,c1,c2,sigma,ro,k1,k2,f,beta)  
+ %TUMOR_ODE defines the ODEs for the Tumor Model including the drug
+   
+ % INPUT parameters:
+    % s is constant immune cells source rate (#cells/day)
+    % sigma is the steepness coefficient (#cells/day)
+    % ro is the tumor recruitment rate of immune cells (1/day) 
+    % c1 is the tumor deactivation rate of immune cells (1/cell*day)
+    % d1 is the natural death rate of immune cells (1/day)
+    % d2 is the natural decay rate of the drug
+    % a is the intrinsic tumor growth rate (1/day)
+    % b is the inverse carrying capacity of tumor population (#cells)
+    % c2 is the immune kill rate of tumor cells (1/cell*day)   
+    % k1 is Drug toxicity to immune cells
+    % k2 is Drug toxicity to tumor cells 
+    % f is the frequency of dosage
+    %beta is the concentration of booster drug
 
-% Tumor model with drug
+     
+ % OUTPUT:
+    % N(1) is the immune cell population = I
+    % N(2) is the tumor cell population = T
+    % N(3) is the drug cycle = U     
+ Np=zeros(size(N));
+    I=N(1);     % Immune cell population
+    T=N(2);     % Tumor cell population
+    U = N(3);   % Chemotherapy drug
+   
+    Np(1) = s+(ro*N(1)*N(2))/(sigma+N(2))-c1*N(1)*N(2)-d1*N(1) - k1*(1-exp(-N(3)));    % Immune cells equation
+    
+    if N(2)>0    
+        Np(2) = a*N(2)*(1-b*N(2))-c2*N(1)*N(2)-k2*(1-exp(-N(3)));% Tumor cells equation
+    else
+        Np(2)=0; % prevents the drug from killing the tumor cells into a negative population-unrealistic
+    end            
+      
+    if t>1 && mod(t,f)<0.25  %determines the  number dosing cycles in time t
+        Np(3) = beta/0.25; %drug booster equation
+      else
+        Np(3) = -d2*N(3); %drug natural decay equation
+      end 
 
-clear all
-close all
-clc
-clf
-%close all
-
-% Input parameter values
-
-s = 0.1181;         % Constant immune cells source rate (#cells/day)
-sigma = 20.19;      % Steepness coefficient (#cells/day)
-ro = 1.131;         % Tumor recruitment rate of immune cells (1/day) 
-c1 = 0.00311;       % Tumor deactivation rate of immune cells (1/cell*day)
-d1 = 0.3743;        % Natural death rate of immune cells (1/day)
-d2 = 2.0;           % Natural rate of decay of drug (1/day)
-a = 1.636;          % Intrinsic tumor growth rate (1/day)
-b = 0.002;          % Inverse carrying capacity of tumor population (#cells)
-c2 = 1;             % Immune kill rate of tumor cells (1/cell*day)
-k1 = 0.05;          % Drug toxicity to immune cells 
-k2 = 1.636;          % Drug toxicity to tumor cells 
-
-
-N10 = 0.001;      % Initial Immune cell population (*10^6)
-N20= 1;           % Initial tumor cell population (tumor burden) (*10^6)
-N30 = 1;          %Initial drug given
-tend = 100;          % Simulation length (time)
-
-N0=[N10 N20 N30];
-
-opts = odeset('MaxStep',1e-2);
-
-[t,N] = ode23s('TUMOR_ODED2',[0 tend],N0,[opts],s,d1,d2,a,b,c1,c2,sigma,ro,k1,k2);
-                                   
-subplot(2,1,1)
-plot(t,N(:,1)); %plots immune cell growth over time
-    xlabel('time')
-    ylabel('Immune cells') 
-
-subplot(2,1,2)
-plot(t,N(:,2),'m'); %plots tumor growth over time
-    xlabel('time')
-    ylabel('tumor cells')
-
-figure %drug over time
-plot(t, N(:,3)) %plots drug over time
-    xlabel('Time')
-    ylabel('Drug delivered')
-
-
-figure 
-
-%Kill rate of Drug
-
-%subplot(2,1,1)
-
-u= linspace(0,10);
-Fu=k2.*(1-exp(-u)); %Fu is the per cell kill rate
-hold on
-plot(u, Fu);
-xlabel('Amount of Drug');
-ylabel('Per tumor cell kill rate');
-
-yline(k2,'r--','Saturation level')
-
-%the solution for Q1a
-
- 
-%  subplot(2,1,2)
-% i = linspace(0,10);
-% Fu=k1.*(1-exp(-u));
-% plot(i, Fu);
-% hold on
-% yline(k1,'r--','Saturation level')
-% % 
-% xlabel('Amount of Drug');
-% ylabel('Per immune cell kill rate');
+         
+end
